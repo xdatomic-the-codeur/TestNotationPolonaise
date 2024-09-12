2c3dfaefa593acbc3fe53e1568fb1ad3e94d2adb
xdatomic-the-codeur <erwann.philippe2@gmail.com>
2024-09-12 15:36:05 +0200

    Ajoutez des fichiers projet.

---
+++ TestNotationPolonaise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TestNotationPolonaise
+{
+    class Program
+    {
+        /// <summary>
+        /// saisie d'une réponse d'un caractère parmi 2
+        /// </summary>
+        /// <param name="message">message à afficher</param>
+        /// <param name="carac1">premier caractère possible</param>
+        /// <param name="carac2">second caractère possible</param>
+        /// <returns>caractère saisi</returns>
+        static char saisie(string message, char carac1, char carac2)
+        {
+            char reponse;
+            do
+            {
+                Console.WriteLine();
+                Console.Write(message + " (" + carac1 + "/" + carac2 + ") ");
+                reponse = Console.ReadKey().KeyChar;
+            } while (reponse != carac1 && reponse != carac2);
+            return reponse;
+        }
+
+        /// <summary>
+        /// Saisie de formules en notation polonaise pour tester la fonction de calcul
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            char reponse;
+            // boucle sur la saisie de formules
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("entrez une formule polonaise en séparant chaque partie par un espace = ");
+                string laFormule = Console.ReadLine();
+                // affichage du résultat
+                Console.WriteLine("Résultat =  " + Polonaise(laFormule));
+                reponse = saisie("Voulez-vous continuer ?", 'O', 'N');
+            } while (reponse == 'O');
+        }
+
+        static float Polonaise(string formule)
+        {
+            String[] t = formule.Split(' ');
+            
+            for(int i=t.Length-1; i>=0; i--)
+            {
+                Console.Write(t[i]);
+            }
+
+            return 1;
+        }
+    }
+}
